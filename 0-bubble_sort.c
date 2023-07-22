60f172e9e560ec7077fdde2d60cba6537a7f855b
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-07-22 08:11:03 -0700

    dazt

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+* bubble_sort - sort
+* @array: array of
+* @size: sizeof
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+   int tmp;
+   size_t i,y;
+   
+    for (i=0; i < size -i -1  ; i++)
+   {
+        for (y=0; y  < size -1 ; y++)
+        {
+            if (array[y] > array[y+1])
+            {
+                tmp = array[y];
+                array[y]= array[y+1];
+                array[y+1]= tmp;
+                 print_array(array, size);
+                
+            }
+        }
+    }
+}
