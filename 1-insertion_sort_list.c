0e723ed7f6c34985ed819a477f9ba4c816e8f5c1
ZAKARIA <zakariaelaro@gmail.com>
2023-07-23 08:02:22 -0700

    check task if work task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,50 +1,45 @@
 #include "sort.h"
 
 /**
+ * swap_nodes - Swap two nodes
+ * @h: A pointe
+ * @n1: A pointer first swap
+ * @n2: The second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+(*n1)->next = n2->next;
+if (n2->next != NULL)
+	n2->next->prev = *n1;
+n2->prev = (*n1)->prev;
+n2->next = *n1;
+if ((*n1)->prev != NULL)
+	(*n1)->prev->next = n2;
+else
+	*h = n2;
+(*n1)->prev = n2;
+*n1 = n2->prev;
+}
+
+/**
  * insertion_sort_list - Sorts a doubly linked list
- *
- * @list: Pointer to a pointer to the head of the list
+ * @list: A pointer to the head
 */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *current = (*list)->next;
-listint_t *temp = *list;
+listint_t *iterr, *insert, *tmp;
+
 if (list == NULL || *list == NULL || (*list)->next == NULL)
-return;
+	return;
 
-while (current != NULL)
+for (iterr = (*list)->next; iterr != NULL; iterr = tmp)
 {
-listint_t *next = current->next;
-listint_t *prev = current->prev;
-int value = current->n;
-
-while (prev != NULL && prev->n > value)
-{
-prev->next = current->next;
-if (current->next != NULL)
-current->next->prev = prev;
-current->next = prev;
-current->prev = prev->prev;
-prev->prev = current;
-
-if (current->prev == NULL)
-*list = current;
-else
-current->prev->next = current;
-
-prev = current->prev;
-}
-
-current = next;
-
-while (temp != NULL)
-{
-printf("%d", temp->n);
-if (temp->next != NULL)
-printf(", ");
-temp = temp->next;
-}
-printf("\n");
+	tmp = iterr->next;
+	insert = iterr->prev;
+	while (insert != NULL && iterr->n < insert->n)
+	{
+		swap_nodes(list, &insert, iterr);
+		print_list((const listint_t *)*list);
+	}
 }
 }
-
