795ea812fb4e6c9e8de4e4d80b1ee1a9f06da588
ZAKARIA <zakariaelaro@gmail.com>
2023-07-23 08:10:02 -0700

    Selection sort Task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,13 +10,13 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
 (*n1)->next = n2->next;
 if (n2->next != NULL)
-	n2->next->prev = *n1;
+        n2->next->prev = *n1;
 n2->prev = (*n1)->prev;
 n2->next = *n1;
 if ((*n1)->prev != NULL)
-	(*n1)->prev->next = n2;
+        (*n1)->prev->next = n2;
 else
-	*h = n2;
+        *h = n2;
 (*n1)->prev = n2;
 *n1 = n2->prev;
 }
@@ -27,19 +27,19 @@ else
 */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *iterr, *insert, *tmp;
+listint_t *iterr, *insert_t, *tm;
 
 if (list == NULL || *list == NULL || (*list)->next == NULL)
-	return;
+        return;
 
-for (iterr = (*list)->next; iterr != NULL; iterr = tmp)
+for (iterr = (*list)->next; iterr != NULL; iterr = tm)
 {
-	tmp = iterr->next;
-	insert = iterr->prev;
-	while (insert != NULL && iterr->n < insert->n)
-	{
-		swap_nodes(list, &insert, iterr);
-		print_list((const listint_t *)*list);
-	}
+        tm = iterr->next;
+        insert_t = iterr->prev;
+        while (insert_t != NULL && iterr->n < insert_t->n)
+        {
+                swap_nodes(list, &insert_t, iterr);
+                print_list((const listint_t *)*list);
+        }
 }
 }
